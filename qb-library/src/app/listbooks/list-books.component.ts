afbfbabcd79f43c2283f436509ff3d5dfe724289
kavitha <kavitha@qburst.com>
2017-02-16 09:59:58 +0530

    updated files related to routing and books json

--- qb-library/src/app/listbooks/list-books.component.ts
+++ qb-library/src/app/listbooks/list-books.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'list-books',
   templateUrl: './list-books.component.html',
-  styleUrls: ['./list-books.component.scss']
+  styleUrls: ['./list-books.component.scss'],
+  
 })
 export class ListBooks {
 }
